a81add52d4485251b0ba6bd4bc5c9a92d051503b
Dirk Ginader <dirk@ginader.com>
2013-03-31 16:14:24 -0700

    add Grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner : '/**' +
+      ' * Copyright (c) 2008 Tom Deater (http://www.tomdeater.com) \n' +
+      ' * Licensed under the MIT License: \n' +
+      ' * http://www.opensource.org/licenses/mit-license.php \n' +
+      ' */',
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      files: ['Gruntfile.js', 'jquery.onfontresize.js']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.files %>',
+        tasks: ['jshint']
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: 'jquery.onfontresize.js',
+        dest: 'jquery.onfontresize.min.js'
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
